1167aa640779db0acc4321599655e98c59d43880
Noah Bice <47279006+noahbice@users.noreply.github.com>
2021-10-21 17:35:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ plot_pdds = False
 ##################
 
 fs_dictionary = {'4x4': 1, '6x6': 2, '6x10': 3, '10x10': 4, '15x15': 5, '20x20': 6, '25x25': 7}
-energy_dictionary = {0: '6 MeV', 1:'9 MeV', 2: '12 MeV', 3:'16 MeV', 4:'20 MeV'}
+energy_dictionary = {0: '6 MeV', 1: '9 MeV', 2: '12 MeV', 3: '16 MeV', 4: '20 MeV'}
 fs_idx = fs_dictionary[field_size]
 data = np.load('pdds.npy', allow_pickle=True)
 
@@ -40,6 +40,7 @@ for i in range(5):
         d_idx = np.where(np.abs(fine_res_dose[d_max_idx:] - 90) == np.amin(np.abs(fine_res_dose[d_max_idx:] - 90)))[0]
         d_idx += d_max_idx
         R90s[i, j] = fine_res_depths[d_idx]
+
 
 def print_out(t_min, t_max, field_size, energy_index, bolus_thickness, plot=False, verbose=True):
     fs_idx = fs_dictionary[field_size]
@@ -91,6 +92,7 @@ def print_out(t_min, t_max, field_size, energy_index, bolus_thickness, plot=Fals
 
     return [t_min_dose, t_max_dose, d_max_dose, skin_dose, d_1pt5tmax]
 
+
 def sunshine_logic(t_min, t_max, field_size):
     fs_idx = fs_dictionary[field_size]
 
@@ -117,27 +119,49 @@ def sunshine_logic(t_min, t_max, field_size):
 
     return recommended_energy, recommended_bolus
 
-def brute_force(t_min, t_max, field_size, w1=1., w2=1., w3=1., w4=1.):
+
+def brute_force(t_min, t_max, field_size, w_t_min=1., w_hotspot=1., w_skin=1., w_depth=1.):
     possibilities = []
     depth_dose = np.round(1.5 * t_max, 2)
     for energy_index in range(5):
         for bolus_thickness in [0, 3, 5, 10]:
             po = print_out(t_min, t_max, field_size, energy_index, bolus_thickness, verbose=False)
-            error = (w1 * (100 - po[0]) ** 2) + (w2 * (100 - po[2]) **2) + (w3 * po[3] ** 2) + (w4 * (po[4]) ** 2)
-            possibilities.append([energy_dictionary[energy_index], bolus_thickness] + po + [error] )
-    possibilities = pd.DataFrame(np.array(possibilities), columns=['Energy', 'Bolus', 'Target Entrance Dose',
+            skin_err = w_skin * (max(90, po[3]) - 90)
+            depth_err = w_depth * (max(50, po[4]) - 50)
+            hotspot_error = w_hotspot * (100 - po[2])
+            w_t_min_error = w_t_min * np.abs(100 - po[0])
+            error = w_t_min_error + hotspot_error + skin_err + depth_err
+            print(energy_dictionary[energy_index], bolus_thickness)
+            print('1', w_t_min * (100 - po[0]))
+            print('2', w_hotspot * (100 - po[2]))
+            print('3', w_skin * (max(90, po[3]) - 90))
+            print('4', w_depth * np.abs(po[4]))
+
+            cont = False
+            for value in po:
+                if value < 200:
+                    continue
+                else:
+                    cont = True
+                    break
+            if cont == True:
+                continue
+            possibilities.append([energy_dictionary[energy_index] + ',  ' + str(bolus_thickness) + ' mm'] + po + [error])
+    possibilities = pd.DataFrame(np.array(possibilities), columns=['Energy, Bolus', 'Target Entrance Dose',
                                                                    'Target Exit Dose', 'Hot Spot', 'Skin Dose',
                                                                    'Depth Dose ({} mm)'.format(depth_dose), 'Error'])
     possibilities['Skin Dose'] = possibilities['Skin Dose'].astype('float').round(2)
     possibilities['Target Exit Dose'] = possibilities['Target Exit Dose'].astype('float').round(2)
     possibilities['Target Entrance Dose'] = possibilities['Target Entrance Dose'].astype('float').round(2)
     possibilities['Hot Spot'] = possibilities['Hot Spot'].astype('float').round(2)
-    possibilities['Depth Dose ({} mm)'.format(depth_dose)] = possibilities['Depth Dose ({} mm)'.format(depth_dose).format(np.round(1.5*t_max, 2))].astype('float').round(2)
+    possibilities['Depth Dose ({} mm)'.format(depth_dose)] = possibilities[
+        'Depth Dose ({} mm)'.format(depth_dose).format(np.round(1.5 * t_max, 2))].astype('float').round(2)
 
     possibilities = possibilities.sort_values(by=['Error'], axis=0, ascending=True)
     possibilities = possibilities.drop(columns=['Error'])
-    
+
     return possibilities
+
 
 # sunshine_logic(t_min, t_max, field_size)
 brute_force(t_min, t_max, field_size)
@@ -146,10 +170,16 @@ st.set_page_config(layout='wide')
 st.title('Electron Energy/Bolus Estimator')
 t_min_input = st.number_input('Target Min Depth [mm]', value=10.)
 t_max_input = st.number_input('Target Max Depth [mm]', value=20.)
+if t_max_input < t_min_input:
+    st.error('Target min depth should be < target max depth.')
+    quit()
+
+
 field_size_input = st.selectbox('Field Size: ', ('4x4', '6x6', '6x10', '10x10', '15x15', '20x20', '25x25'), index=3)
 output = brute_force(t_min_input, t_max_input, field_size_input)
-# st.dataframe(output, width=2000, height=1000)
-st.table(output)
+output = output.set_index(['Energy, Bolus'])
+st.dataframe(output, width=2000, height=1000)
+# st.table(output)
 
 colors = ['royalblue', 'darkgoldenrod', 'green', 'darkred', 'coral', 'orchid', 'lightgreen', 'navy']
 boluses = [0, 3, 5, 10]
@@ -159,9 +189,10 @@ for bt in range(4):
         interpolant = interp1d(data[e][:, 0], data[e][:, fs_idx])
         fine_res_depths = np.linspace(0, data[e][-1, 0], 1000)
         fine_res_dose = interpolant(fine_res_depths)
-        t_max_idx = np.where(np.abs(fine_res_depths - (t_max_input + boluses[bt])) == np.amin(np.abs(fine_res_depths - (t_max_input + boluses[bt]))))[0][0]
+        t_max_idx = np.where(np.abs(fine_res_depths - (t_max_input + boluses[bt])) == np.amin(
+            np.abs(fine_res_depths - (t_max_input + boluses[bt]))))[0][0]
         t_max_dose = fine_res_dose[t_max_idx]
-        
+
         rx_normed = (data[e][:, fs_idx] / t_max_dose) * 100
         if np.amax(rx_normed) > 150:
             continue
@@ -172,11 +203,10 @@ for bt in range(4):
         axs.plot([boluses[bt], boluses[bt]], [0, 150], label='skin surface', color=colors[-3])
     axs.set_xlim([0, np.minimum(3 * t_max_input, data[e][-1, 0])])
     axs.set_ylim([0, 150])
-        
+
     axs.set_title('{} mm bolus'.format(boluses[bt]))
     axs.legend(fontsize='x-small', loc='upper right')
     axs.set_xlabel('Depth')
     axs.set_ylabel('Percent Rx Dose')
     fig.savefig('{}PDD.png'.format(boluses[bt]))
     st.image('{}PDD.png'.format(boluses[bt]))
-
